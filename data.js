f7dcce3700896365aaccf4c5bbda129c80bac363
Chile360VirtualTours <chile360virtualtours@gmail.com>
2023-10-16 23:25:00 -0600

    Upload

---
+++ data.js
@@ -0,0 +1,464 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "ENTRADA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.835756078007819,
+        "pitch": 0.3441541911456625,
+        "fov": 1.2989219527417935
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.450942761576977,
+          "pitch": 0.9280641115490162,
+          "rotation": 0,
+          "target": "1-living-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.52550644731987,
+          "pitch": 0.11555866960151917,
+          "title": "TABLERO ELECTRICO",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-living-1",
+      "name": "LIVING 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.563437822497944,
+        "pitch": 0.37361474417824425,
+        "fov": 1.2989219527417935
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6710197391348487,
+          "pitch": 0.8561072859864325,
+          "rotation": 0,
+          "target": "2-cocina"
+        },
+        {
+          "yaw": -0.13252070441216013,
+          "pitch": 0.8874769625618235,
+          "rotation": 0,
+          "target": "0-entrada"
+        },
+        {
+          "yaw": 2.5021745777678763,
+          "pitch": 0.836613193996051,
+          "rotation": 0,
+          "target": "3-living-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9120795668375781,
+          "pitch": 0.05533111933419832,
+          "title": "TABLERO ELECTRICO",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-cocina",
+      "name": "COCINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.10275216055579861,
+        "pitch": 0.17628226501497046,
+        "fov": 1.2989219527417935
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.857438514322112,
+          "pitch": 0.9858543311046066,
+          "rotation": 6.283185307179586,
+          "target": "3-living-2"
+        },
+        {
+          "yaw": -2.127742246182457,
+          "pitch": 0.8813483931871513,
+          "rotation": 0,
+          "target": "1-living-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3179029979843513,
+          "pitch": 0.05837434088320137,
+          "title": "LOGIA",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-living-2",
+      "name": "LIVING 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.073035914917414,
+        "pitch": 0.2808534994753664,
+        "fov": 1.2989219527417935
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2706725068899587,
+          "pitch": 0.7658865342557739,
+          "rotation": 6.283185307179586,
+          "target": "4-sala-de-estar"
+        },
+        {
+          "yaw": 3.0580290938272245,
+          "pitch": 0.9407708558336445,
+          "rotation": 0,
+          "target": "5-pasillo-1"
+        },
+        {
+          "yaw": -0.2581435634324798,
+          "pitch": 0.9274403060876804,
+          "rotation": 0,
+          "target": "2-cocina"
+        },
+        {
+          "yaw": -1.2051030866076982,
+          "pitch": 0.797632877354955,
+          "rotation": 0,
+          "target": "1-living-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sala-de-estar",
+      "name": "SALA DE ESTAR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.6246686327567534,
+        "pitch": 0.444585183781129,
+        "fov": 1.2989219527417935
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1214282243887421,
+          "pitch": 0.8388977056877778,
+          "rotation": 0,
+          "target": "3-living-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pasillo-1",
+      "name": "PASILLO 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.06777270164322324,
+        "pitch": 0.25669171923230216,
+        "fov": 1.2989219527417935
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08758176875393886,
+          "pitch": 0.8941225112106501,
+          "rotation": 0,
+          "target": "3-living-2"
+        },
+        {
+          "yaw": 3.1313953563449637,
+          "pitch": 0.7620943484751734,
+          "rotation": 0,
+          "target": "6-pasillo-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pasillo-2",
+      "name": "PASILLO 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.9206863593011487,
+        "pitch": 0.2148437667131926,
+        "fov": 1.2989219527417935
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1290531388003444,
+          "pitch": 0.7054209926367339,
+          "rotation": 0,
+          "target": "5-pasillo-1"
+        },
+        {
+          "yaw": 2.0310048907380978,
+          "pitch": 0.633438444169105,
+          "rotation": 0,
+          "target": "7-habitacion-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-habitacion-1",
+      "name": "HABITACION 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.024765102296307,
+        "pitch": 0.3412168986939861,
+        "fov": 1.2989219527417935
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5536940960459091,
+          "pitch": 0.8067112035648432,
+          "rotation": 0,
+          "target": "8-habitacion-2"
+        },
+        {
+          "yaw": -1.5087239333445588,
+          "pitch": 0.7262470826585066,
+          "rotation": 0,
+          "target": "6-pasillo-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-habitacion-2",
+      "name": "HABITACION 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.4607974212541883,
+        "pitch": 0.3030924238260031,
+        "fov": 1.2989219527417935
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4786146981397437,
+          "pitch": 0.7900668960463477,
+          "rotation": 0,
+          "target": "7-habitacion-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
